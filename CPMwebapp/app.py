61cbd6d8e99694309819319528710d925a6389c0
TheStarCloud <91798508+TheStarCloud@users.noreply.github.com>
2023-05-07 02:59:43 -0700

    Update app.py

--- CPMwebapp/app.py
+++ CPMwebapp/app.py
@@ -1,24 +1,31 @@
-from flask import Flask, request
+from flask import Flask, render_template, request, session
+
 app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
 
 @app.route('/')
 def home():
-    return '''
-        <form method="POST" action="/calculate">
-            <label>Impressions:</label>
-            <input type="text" name="impressions"><br>
-            <label>Media Cost:</label>
-            <input type="text" name="media_cost"><br>
-            <input type="submit" value="Calculate">
-        </form>
-    '''
+    return render_template('index.html')
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
     impressions = int(request.form['impressions'])
     media_cost = float(request.form['media_cost'])
     cpm = (media_cost / impressions) * 1000
-    return f"The CPM is {cpm:.2f}"
+    session['cpm'] = cpm
+    return render_template('index.html')
+
+@app.before_request
+def before_request():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=5)
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 if __name__ == '__main__':
     app.run()
